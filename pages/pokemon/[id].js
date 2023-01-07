391fca0625c9e00defe5b8509736d945589152a2
Folka2134 <folka2134@gmail.com>
2023-01-07 12:14:45 +0000

    Moving to SSR

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -1,32 +1,20 @@
-import { useRouter } from "next/router";
 import Head from "next/head";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 import styles from "../../styles/Details.module.css";
 
+export async function getServerSideProps({ params }) {
+  const res = await fetch(
+    `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${id}.json`
+  );
+
+  return {
+    props: {
+      pokemon: await res.json(),
+    },
+  };
+}
+
 export default function Details() {
-  const {
-    query: { id },
-  } = useRouter();
-
-  const [pokemon, setPokemon] = useState(null);
-
-  useEffect(() => {
-    async function getPokemon() {
-      const res = await fetch(
-        `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${id}.json`
-      );
-      setPokemon(await res.json());
-    }
-    if (id) {
-      getPokemon();
-    }
-  }, [id]);
-
-  if (!pokemon) {
-    return null;
-  }
-
   return (
     <div>
       <Head>
